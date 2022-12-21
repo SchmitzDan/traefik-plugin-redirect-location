b489edf27dc56a7069daf08b1de8192c2e4a46be
Daniel Schmitz <daniel.schmitz@mail.de>
2022-12-21 15:43:17 +0100

    chore: fix linting errors

--- redirect_location.go
+++ redirect_location.go
@@ -1,4 +1,4 @@
-// Package traefik_plugin_redirect_location is a traefik plugin fixing the location header in a redirect response
+// Package traefik_plugin_redirect_location is a traefik plugin fixing the location header in a redirect response.
 package traefik_plugin_redirect_location //nolint
 import (
 	"context"
@@ -12,7 +12,7 @@ import (
 
 const locationHeader string = "Location"
 
-// Definition of a replacement.
+// Rewrite definition of a replacement.
 type Rewrite struct {
 	Regex       string `json:"regex,omitempty" toml:"regex,omitempty" yaml:"regex,omitempty"`
 	Replacement string `json:"replacement,omitempty" toml:"replacement,omitempty" yaml:"replacement,omitempty"`
@@ -29,7 +29,7 @@ func CreateConfig() *Config {
 	return &Config{}
 }
 
-// RedirectLocation a traefik plugin fixing the location header in a redirect response
+// RedirectLocation a traefik plugin fixing the location header in a redirect response.
 type RedirectLocation struct {
 	defaultHandling bool
 	rewrites        []rewrite
@@ -42,7 +42,7 @@ type rewrite struct {
 	replacement string
 }
 
-// create a RedirectLocation plugin instance
+// New create a RedirectLocation plugin instance.
 func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
 	rewrites := make([]rewrite, len(config.Rewrites))
 
@@ -67,18 +67,18 @@ func New(_ context.Context, next http.Handler, config *Config, name string) (htt
 
 func (r *RedirectLocation) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	myWriter := &responseWriter{
-		defaultHandling: r.defaultHandling,
-		rewrites:        r.rewrites,
-		writer:          rw,
-		request:         req,
+		defaultHandlingEnabled: r.defaultHandling,
+		rewrites:               r.rewrites,
+		writer:                 rw,
+		request:                req,
 	}
 
 	r.next.ServeHTTP(myWriter, req)
 }
 
 type responseWriter struct {
-	defaultHandling bool
-	rewrites        []rewrite
+	defaultHandlingEnabled bool
+	rewrites               []rewrite
 
 	writer  http.ResponseWriter
 	request *http.Request
@@ -92,6 +92,42 @@ func (r *responseWriter) Write(bytes []byte) (int, error) {
 	return r.writer.Write(bytes)
 }
 
+func (r *responseWriter) defaultHandling(location string) string {
+	locationURL, err := url.Parse(location)
+	if err != nil {
+		http.Error(r.writer, err.Error(), http.StatusInternalServerError)
+		return ""
+	}
+
+	host := r.request.Header.Get("X-Forwarded-Host")
+
+	if locationURL.Host == host || locationURL.Host == "" {
+		// path prefix
+		prefix := r.request.Header.Get("X-Forwarded-Prefix")
+		if strings.HasPrefix(strings.TrimPrefix(locationURL.Path, "/"), prefix) {
+			// it seems the service has handled the removed prefix correct so do nothing
+		} else {
+			oldPath := locationURL.Path
+			locationURL.Path = path.Join(prefix, locationURL.Path)
+			// some logging
+			fmt.Println("Changed location path from ", oldPath, "to", locationURL.Path)
+		}
+	}
+
+	return locationURL.String()
+}
+
+func (r *responseWriter) handleRewrites(location string) string {
+	for _, rewrite := range r.rewrites {
+		locationOld := location
+		location = rewrite.regex.ReplaceAllString(location, rewrite.replacement)
+		// some logging
+		fmt.Println("Changed location from ", locationOld, "to", location)
+	}
+
+	return location
+}
+
 func (r *responseWriter) WriteHeader(statusCode int) {
 	// only manipulate if redirect
 	if statusCode > 300 && statusCode < 400 {
@@ -101,43 +137,16 @@ func (r *responseWriter) WriteHeader(statusCode int) {
 		location := r.writer.Header().Get(locationHeader)
 
 		// default handling
-		if r.defaultHandling {
-
-			locationUrl, err := url.Parse(location)
-			if err != nil {
-				http.Error(r.writer, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			host := r.request.Header.Get("X-Forwarded-Host")
-
-			if locationUrl.Host == host || locationUrl.Host == "" {
-
-				// path prefix
-				prefix := r.request.Header.Get("X-Forwarded-Prefix")
-				if strings.HasPrefix(strings.TrimPrefix(locationUrl.Path, "/"), prefix) {
-					// it seems the service has handled the removed prefix correct so do nothing
-				} else {
-					oldPath := locationUrl.Path
-					locationUrl.Path = path.Join(prefix, locationUrl.Path)
-					// some logging
-					fmt.Println("Changed location path from ", oldPath, "to", locationUrl.Path)
-				}
-			}
-
-			location = locationUrl.String()
+		if r.defaultHandlingEnabled {
+			location = r.defaultHandling(location)
 		}
 
 		// rewrites
-		for _, rewrite := range r.rewrites {
-			locationOld := location
-			location = rewrite.regex.ReplaceAllString(location, rewrite.replacement)
-			// some logging
-			fmt.Println("Changed location from ", locationOld, "to", location)
+		if len(r.rewrites) > 0 {
+			location = r.handleRewrites(location)
 		}
 
 		r.writer.Header().Set(locationHeader, location)
-
 	}
 
 	// call the wrapped writer
