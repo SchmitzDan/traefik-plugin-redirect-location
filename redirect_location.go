79171d697ad7f525e2c1331b594e2ddc270ebf2c
Daniel Schmitz <67006107+SchmitzDan@users.noreply.github.com>
2022-12-22 12:25:02 +0100

    feat: Initial implementation
    * ci: ci pipeline
    
    * feat: initial implementation
    
    * chore: add go.mod
    
    * chore: fix linting errors
    
    * fix: linting error (whitespace)
    
    * test: Add some unit tests
    
    * fix: Comparing of hostname and leading slashes
    
    * docs: Write Readme
    
    * fix: linting errors

---
+++ redirect_location.go
@@ -0,0 +1,153 @@
+// Package traefik_plugin_redirect_location is a traefik plugin fixing the location header in a redirect response.
+package traefik_plugin_redirect_location //nolint
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
+)
+
+const locationHeader string = "Location"
+
+// Rewrite definition of a replacement.
+type Rewrite struct {
+	Regex       string `json:"regex,omitempty" toml:"regex,omitempty" yaml:"regex,omitempty"`
+	Replacement string `json:"replacement,omitempty" toml:"replacement,omitempty" yaml:"replacement,omitempty"`
+}
+
+// Config of the plugin.
+type Config struct {
+	Default  bool      `json:"default" toml:"default" yaml:"default"`
+	Rewrites []Rewrite `json:"rewrites,omitempty" toml:"rewrites,omitempty" yaml:"rewrites,omitempty"`
+}
+
+// CreateConfig creates and initializes the plugin configuration.
+func CreateConfig() *Config {
+	return &Config{}
+}
+
+// RedirectLocation a traefik plugin fixing the location header in a redirect response.
+type RedirectLocation struct {
+	defaultHandling bool
+	rewrites        []rewrite
+	next            http.Handler
+	name            string
+}
+
+type rewrite struct {
+	regex       *regexp.Regexp
+	replacement string
+}
+
+// New create a RedirectLocation plugin instance.
+func New(_ context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	rewrites := make([]rewrite, len(config.Rewrites))
+
+	for i, rewriteConfig := range config.Rewrites {
+		regexp, err := regexp.Compile(rewriteConfig.Regex)
+		if err != nil {
+			return nil, fmt.Errorf("error compiling regex %q: %w", rewriteConfig.Regex, err)
+		}
+		rewrites[i] = rewrite{
+			regex:       regexp,
+			replacement: rewriteConfig.Replacement,
+		}
+	}
+
+	return &RedirectLocation{
+		defaultHandling: config.Default,
+		rewrites:        rewrites,
+		next:            next,
+		name:            name,
+	}, nil
+}
+
+func (r *RedirectLocation) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	myWriter := &responseWriter{
+		defaultHandlingEnabled: r.defaultHandling,
+		rewrites:               r.rewrites,
+		writer:                 rw,
+		request:                req,
+	}
+
+	r.next.ServeHTTP(myWriter, req)
+}
+
+type responseWriter struct {
+	defaultHandlingEnabled bool
+	rewrites               []rewrite
+
+	writer  http.ResponseWriter
+	request *http.Request
+}
+
+func (r *responseWriter) Header() http.Header {
+	return r.writer.Header()
+}
+
+func (r *responseWriter) Write(bytes []byte) (int, error) {
+	return r.writer.Write(bytes)
+}
+
+func (r *responseWriter) defaultHandling(location string) string {
+	locationURL, err := url.Parse(location)
+	if err != nil {
+		http.Error(r.writer, err.Error(), http.StatusInternalServerError)
+		return ""
+	}
+
+	host := r.request.Header.Get("X-Forwarded-Host")
+
+	if locationURL.Hostname() == host || locationURL.Host == "" {
+		// path prefix
+		prefix := r.request.Header.Get("X-Forwarded-Prefix")
+		if strings.HasPrefix(strings.TrimPrefix(locationURL.Path, "/"), strings.TrimPrefix(prefix, "/")) {
+			// it seems the service has handled the removed prefix correct so do nothing
+		} else {
+			oldPath := locationURL.Path
+			locationURL.Path = path.Join(prefix, locationURL.Path)
+			// some logging
+			fmt.Println("Changed location path from ", oldPath, "to", locationURL.Path)
+		}
+	}
+
+	return locationURL.String()
+}
+
+func (r *responseWriter) handleRewrites(location string) string {
+	for _, rewrite := range r.rewrites {
+		locationOld := location
+		location = rewrite.regex.ReplaceAllString(location, rewrite.replacement)
+		// some logging
+		fmt.Println("Changed location from ", locationOld, "to", location)
+	}
+
+	return location
+}
+
+func (r *responseWriter) WriteHeader(statusCode int) {
+	// only manipulate if redirect
+	if statusCode > 300 && statusCode < 400 {
+		// get header value
+		// as we are handling a redirect there should be one and only one location header
+		location := r.writer.Header().Get(locationHeader)
+
+		// default handling
+		if r.defaultHandlingEnabled {
+			location = r.defaultHandling(location)
+		}
+
+		// rewrites
+		if len(r.rewrites) > 0 {
+			location = r.handleRewrites(location)
+		}
+
+		r.writer.Header().Set(locationHeader, location)
+	}
+
+	// call the wrapped writer
+	r.writer.WriteHeader(statusCode)
+}
---
+++ redirect_location_test.go
@@ -0,0 +1,166 @@
+// Package traefik_plugin_redirect_location is a traefik plugin fixing the location header in a redirect response
+package traefik_plugin_redirect_location //nolint
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRewrites(t *testing.T) {
+	tests := []struct {
+		desc           string
+		rewrites       []Rewrite
+		locationBefore string
+		expLocation    string
+	}{
+		{
+			desc: "should replace foo by bar in location header",
+			rewrites: []Rewrite{
+				{
+					Regex:       "foo",
+					Replacement: "bar",
+				},
+			},
+			locationBefore: "foo",
+			expLocation:    "bar",
+		},
+		{
+			desc: "should replace foo by bar in location header",
+			rewrites: []Rewrite{
+				{
+					Regex:       "foo",
+					Replacement: "bar",
+				},
+			},
+			locationBefore: "prefix/foo/path",
+			expLocation:    "prefix/bar/path",
+		},
+		{
+			desc: "should replace http by https in location header",
+			rewrites: []Rewrite{
+				{
+					Regex:       "^http://(.+)$",
+					Replacement: "https://$1",
+				},
+			},
+			locationBefore: "http://test:1000",
+			expLocation:    "https://test:1000",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			config := &Config{
+				Default:  false,
+				Rewrites: test.rewrites,
+			}
+
+			next := func(rw http.ResponseWriter, req *http.Request) {
+				rw.Header().Add("Location", test.locationBefore)
+				rw.WriteHeader(http.StatusMovedPermanently)
+			}
+
+			redirectLocation, err := New(context.Background(), http.HandlerFunc(next), config, "redirectLocation")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			recorder := httptest.NewRecorder()
+
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+			redirectLocation.ServeHTTP(recorder, req)
+
+			location := recorder.Header().Get("Location")
+
+			if test.expLocation != location {
+				t.Errorf("Unexpected redirect Location: expected %+v, result: %+v", test.expLocation, location)
+			}
+		})
+	}
+}
+
+func TestDefaultHandling(t *testing.T) {
+	tests := []struct {
+		desc            string
+		forwardedPrefix string
+		forwardedHost   string
+		locationBefore  string
+		expLocation     string
+	}{
+		{
+			desc:           "No forwarded Prefix and relative path",
+			locationBefore: "somevalue",
+			expLocation:    "somevalue",
+		},
+		{
+			desc:           "No forwarded Prefix and absolute path",
+			locationBefore: "http://host:815/path",
+			expLocation:    "http://host:815/path",
+		},
+		{
+			desc:            "Forwarded Prefix and relative path",
+			forwardedPrefix: "/test",
+			locationBefore:  "somevalue",
+			expLocation:     "/test/somevalue",
+		},
+		{
+			desc:            "Forwarded Prefix and relative path already containing prefix",
+			forwardedPrefix: "/test",
+			locationBefore:  "/test/somevalue",
+			expLocation:     "/test/somevalue",
+		},
+		{
+			desc:            "Forwarded Prefix and absolute path",
+			forwardedPrefix: "/test",
+			forwardedHost:   "host",
+			locationBefore:  "http://host:815/path",
+			expLocation:     "http://host:815/test/path",
+		},
+		{
+			desc:            "Forwarded Prefix and absolute path already containing prefix",
+			forwardedPrefix: "/test",
+			forwardedHost:   "host",
+			locationBefore:  "http://host:815/test/path",
+			expLocation:     "http://host:815/test/path",
+		},
+	}
+
+	config := &Config{
+		Default:  true,
+		Rewrites: make([]Rewrite, 0),
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			next := func(rw http.ResponseWriter, req *http.Request) {
+				rw.Header().Add("Location", test.locationBefore)
+				rw.WriteHeader(http.StatusMovedPermanently)
+			}
+
+			redirectLocation, err := New(context.Background(), http.HandlerFunc(next), config, "redirectLocation")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			recorder := httptest.NewRecorder()
+
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			if len(test.forwardedPrefix) > 0 {
+				req.Header.Add("X-Forwarded-Prefix", test.forwardedPrefix)
+			}
+			if len(test.forwardedHost) > 0 {
+				req.Header.Add("X-Forwarded-Host", test.forwardedHost)
+			}
+
+			redirectLocation.ServeHTTP(recorder, req)
+
+			location := recorder.Header().Get("Location")
+
+			if test.expLocation != location {
+				t.Errorf("Unexpected redirect Location: expected %+v, result: %+v", test.expLocation, location)
+			}
+		})
+	}
+}
